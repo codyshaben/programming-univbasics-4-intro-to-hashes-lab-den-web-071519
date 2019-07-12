3dab41983184721452f1aba4560eb33b4da948cd
Cody Shaben <cshaben87@gmail.com>
2019-07-12 16:15:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-   if hash[key]
-    hash[key] = key += 1
+   if hash[key] = value
+    hash[key] += 1
     else
     hash[key] =  1
   end
